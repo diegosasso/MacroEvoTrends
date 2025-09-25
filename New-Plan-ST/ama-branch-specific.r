88dd259f7bffc663c9321800e145d7250228fd2c
Sergei Tarasov <sergxf@yandex.ru>
2025-09-25 15:59:49 +0300

    ama branches

--- New-Plan-ST/ama-branch-specific.r
+++ New-Plan-ST/ama-branch-specific.r
@@ -17,34 +17,31 @@ char3 <- sim.history(tree, Q)$states
 
 
 # 4. Fit a model for each character and generate stochastic maps
-#    We'll use make.simmap() with 100 replicates
-maps1 <- make.simmap(tree, char1, model = "SYM", nsim = 50)
-maps2 <- make.simmap(tree, char2, model = "SYM", nsim = 50)
-maps3 <- make.simmap(tree, char3, model = "SYM", nsim = 50)
+maps1 <- make.simmap(tree, char1, model = "SYM", nsim = 5)
+maps2 <- make.simmap(tree, char2, model = "SYM", nsim = 5)
+maps3 <- make.simmap(tree, char3, model = "SYM", nsim = 5)
 
 plot(maps1[[2]])
 edgelabels()
 
+# Cranium with 3 chars each char with 5 maps
 cranium <- list(char1=maps1, char2=maps2, char3=maps3)
 cranium
 
-#list.of.chars <- cranium
+# This gives a sample of amalgamated rates across characters
 cranium.branch.sample <- ama_bracnhes_multi_char(cranium)
 cranium.branch.sample
-summarize_branch_rates_multi(cranium, 'mean')
 
-m1 <- summarize_branch_rates(cranium$char1, 'mean')
-m2 <- summarize_branch_rates(cranium$char2, 'mean')
-m3 <- summarize_branch_rates(cranium$char3, 'mean')
+# This amalgamates and summarizes rates across characters
+mean.cranium <- summarize_branch_rates_multi(cranium, 'mean')
 
-m1+m2+m3
-summarize_branch_rates_multi(cranium, 'mean')
+# This summarizes rates for one individual character
+mu1 <- summarize_branch_rates(cranium$char1, 'mean')
+mu2 <- summarize_branch_rates(cranium$char2, 'mean')
+mu3 <- summarize_branch_rates(cranium$char3, 'mean')
+
+mu1+mu2+mu3
+mean.cranium
 
 
 
-m1 <- summarize_branch_rates(cranium$char1, 'median')
-m2 <- summarize_branch_rates(cranium$char2, 'median')
-m3 <- summarize_branch_rates(cranium$char3, 'median')
-
-m1+m2+m3
-summarize_branch_rates_multi(cranium, 'median')
